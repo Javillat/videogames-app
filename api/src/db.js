cdf22bf30c30b94dfdc791fa4a179f861ad0b7ea
Javier <wemakesoftx@gmail.com>
2022-09-10 12:29:54 -0600

    chore: add models relationship

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { Videogame, Genro } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Videogame.belongsToMany(Genro, {through: VideogameGenro});
+Genro.belongsToMany(Videogame, {through: VideogameGenro});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
