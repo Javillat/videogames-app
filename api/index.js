41e9e530bc7ee379355cdcc601d9136b423397fc
Javier <wemakesoftx@gmail.com>
2022-09-12 23:22:04 -0600

    chore: add chargeGenders call

--- api/index.js
+++ api/index.js
@@ -19,10 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { chargeGenres } = require('../api/src/controllers/genrofn')
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
+    chargeGenres();
     console.log('%s listening, ready to serve', 'http://localhost:3001'); // eslint-disable-line no-console
   });
 });
