8918c485c891c00e89dd12e94730bbbaba806700
Javier <wemakesoftx@gmail.com>
2022-09-12 23:31:07 -0600

    chore: update route to postVideogames

--- api/src/routes/routesvideogames.js
+++ api/src/routes/routesvideogames.js
@@ -4,5 +4,7 @@ const videogame = require('../controllers/videogamesfn.js');
 const routerfn = Router();
 
 routerfn.get('/', videogame.allVideogames);
+routerfn.get('/:id', videogame.videogameById);
+routerfn.post('/', videogame.postVideogames);
 
 module.exports = routerfn;
