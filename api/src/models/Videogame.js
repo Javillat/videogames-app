3653a292773e19d45851016475a001c0d51ba923
Javier <wemakesoftx@gmail.com>
2022-09-14 10:14:14 -0600

    chore: update platforms datatype array of string (DataTypes.ARRAY(DataTypes.STRING))

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -25,7 +25,7 @@ module.exports = (sequelize) => {
       type: DataTypes.DOUBLE,
     },
     platforms: {
-      type: DataTypes.STRING,
+      type: DataTypes.ARRAY(DataTypes.STRING),
     },
     image: {
       type: DataTypes.STRING,
