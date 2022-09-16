08837564152fbf2cb339cb29da28fb42ea8068b2
Javier <wemakesoftx@gmail.com>
2022-09-15 23:09:40 -0600

    chore: add switch and route to landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './components/Landing';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Videogames</h1>
+      <Switch>
+        <Route exact path='/'><LandingPage /></Route>
+      </Switch>
     </div>
   );
 }
