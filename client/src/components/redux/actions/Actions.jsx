e56180fedf44e7659a2fc4f2f5e3de68b0128a54
Javier <wemakesoftx@gmail.com>
2022-09-19 18:25:38 -0600

    chore: add getvideogamedetail to actions

--- client/src/components/redux/actions/Actions.jsx
+++ client/src/components/redux/actions/Actions.jsx
@@ -9,3 +9,23 @@ export function getVideogames(){
         })
     }
 }
+
+export function getVideogamesByName(gameName){
+    return async (dispatch) => {
+        const getData = await axios.get(`http://localhost:3001/videogames?name=${gameName}`)
+        dispatch({
+            type: 'GET_VIDEOGAME_BY_NAME',
+            payload: getData.data
+        })
+    }
+}
+
+export function getVideogameDetail(id){
+    return async(dispatch) => {
+        const getData = await axios.get(`http://localhost:3001/videogames/${id}`)
+        dispatch({
+            type: 'GET_VIDEOGAME_DETAIL',
+            payload: getData.data
+        })
+    }
+}
