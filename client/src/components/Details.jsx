b127d4ff720fed17daa2cca9ec244375139497c4
Javier <wemakesoftx@gmail.com>
2022-09-20 10:05:09 -0600

    refactor: Change capital first letter

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -26,14 +26,14 @@ export default function Details(){
         (<div>
             <span><button onClick={backHandler}>Go Back</button></span>
             <div><h2>Videogame</h2></div>
-            <div><h3>id</h3>{detail.id}</div>
-            <div><h3>name</h3>{detail.name}</div>
+            <div><h3>Id</h3>{detail.id}</div>
+            <div><h3>Name</h3>{detail.name}</div>
             <div><img src={detail.image} alt="Videogame img" /></div>
-            <div><h3>genres</h3>{detail.genres}</div>
-            <div><h3>dedscription</h3>{detail.description}</div>
-            <div><h3>released</h3>{detail.released}</div>
-            <div><h3>rating</h3>{detail.rating}</div>
-            <div><h3>platforms</h3>{detail.platforms}</div>
+            <div><h3>Genres</h3>{detail.genres}</div>
+            <div><h3>Description</h3>{detail.description}</div>
+            <div><h3>Released</h3>{detail.released}</div>
+            <div><h3>Rating</h3>{detail.rating}</div>
+            <div><h3>Platforms</h3>{detail.platforms}</div>
         </div>
 
         ) : ('Loading')
