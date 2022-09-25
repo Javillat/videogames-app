92697c3d94df7bdfe2a423952ca3e593bdd7d603
Javier <wemakesoftx@gmail.com>
2022-09-25 11:22:35 -0600

    fix: change conditional render

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import Games from "./Games";
+import Loading from "./Loading";
 import Nav from "./Nav";
 import { getVideogames } from './redux/actions/Actions';
+//import '../css/Home.css'
 
 export default function Home(){
+    const [isFilter, setFilter] = useState([]);
+    //const [isVideogames, setVideogames] = useState(false);
     const dispatch = useDispatch();
-    const videogames = useSelector(data => data.videogames);
+    const videogames = useSelector((data) => data.videogames);
 
     useEffect(()=>{
         dispatch(getVideogames())
     },[]);
+    console.log(videogames);
+    if(videogames == undefined || !videogames.length ) return <Loading />;
     return(
         <div>
             <Nav />
