c4164ece1f7319e7fb97881cc1ba0c91edc36a40
Javier <wemakesoftx@gmail.com>
2022-09-27 18:34:22 -0600

    feat: fill select filter with usestate from db

--- client/src/components/SelectOrder.jsx
+++ client/src/components/SelectOrder.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import axios from "axios";
 //import OrderByName from "./helpers/Selecters";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { orderByNameAction, orderByRatingAction } from "./redux/actions/Actions";
-
+import { orderByNameAction, orderByRatingAction, filterByGenre } from "./redux/actions/Actions";
 
 export default function SelectOrder(props){
     const dispatch = useDispatch()
-    //const ordered = useSelector(data => data.videogames)
+    const [genros, setGenros] = useState([]);
 
     function orderByName(event){
         event.preventDefault()
@@ -23,22 +24,46 @@ export default function SelectOrder(props){
         props.setCurrentPage(2);
         else props.setCurrentPage(1);
     }
+    
+    useEffect(()=>{
+        axios.get('http://localhost:3001/genres').then((response) =>{
+             setGenros(response.data);
+             console.log('useeffect ',genros);
+         })
+    },[])
 
     return(
         <div className="order_selector">
-            <select className="select_order_name" 
-            onChange={(event)=> orderByName(event)}>
-                <option value="" defaultValue="">Sort by Name</option>
-                <option value="DESCENDENT">Descendent</option>
-                <option value="ASCENDENT">Ascendent</option>
-            </select>
+            <section>
+                <select className="select_order_name" 
+                onChange={(event)=> orderByName(event)}>
+                    <option value="" defaultValue="">Sort by Name</option>
+                    <option value="DESCENDENT">Descendent</option>
+                    <option value="ASCENDENT">Ascendent</option>
+                </select>
+            </section>
 
-            <select name="selec_order_rating" 
-            onChange={(event) => orderByRating(event)}>
-                <option value="" defaultValue="">Sort by Rating</option>
-                <option value="DESCENDENT">Descendent</option>
-                <option value="ASCENDENT">Ascendent</option>
-            </select>
+            <section>
+                <select name="selec_order_rating" 
+                onChange={(event) => orderByRating(event)}>
+                    <option value="" defaultValue="">Sort by Rating</option>
+                    <option value="DESCENDENT">Descendent</option>
+                    <option value="ASCENDENT">Ascendent</option>
+                </select>
+            </section>
+
+            <section>
+                <select name="select_genre" 
+                onChange={(event) => filterByGenre(event)}>
+                    <option value="" defaultValue="">Filter by Genro</option>
+                    {console.log('select',genros)}
+                    {genros.map(genro => ( 
+                        <option key={genro.id} value={genro.name}>{genro.name}</option>
+                    ))
+                    }
+
+                </select>
+            </section>
         </div>
     )
 }
